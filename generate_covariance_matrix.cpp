7e8f5cd2a738481ef4423a5ced5390a2e4c694f6
cinquemb <cinque_mcfarlane-blake@brown.edu>
2016-04-10 10:40:53 -0400

    removing boost dependencies

--- generate_covariance_matrix.cpp
+++ generate_covariance_matrix.cpp
@@ -6,18 +6,7 @@
 #include <functional>
 #include <map>
 #include <chrono>
-
 #include <armadillo>
-
-#include <boost/algorithm/string.hpp>
-
-const int offset_node = 0;
-
-//window size
-const int window_size = 2048;
-
-//factor to convert biosemi values into uv
-double biosemi_microvoltage_factor = 8192;
 
 //num_colums
 int num_colums = 128;
@@ -105,46 +94,9 @@ arma::mat whiten_matrix_samples(arma::mat& sample_matrix){
         arma::svd(u, simga_matrix, v_matrix, col_norm_sample_matrix);
     else
         arma::svds(u, simga_matrix, v_matrix, arma::sp_mat(col_norm_sample_matrix), n_components);
-    
+
     arma::mat whiten_matrix = u * v_matrix.t();
     return whiten_matrix;
-}
-
-arma::mat matrix_from_file_samples(std::string& file_name){
-	std::vector<std::string> data_vector_string;
-	std::vector<double> data_vector_out;
-	std::string line;
-
-	int line_count = 0;
-
-	std::ifstream in(file_name.c_str());
-	if (!in.is_open()) exit(0);
-
-	arma::mat sample_matrix(window_size, num_colums);
-
-	while (std::getline(in,line)){
-		if(line_count >= window_size)
-			break;
-
-    	data_vector_string.clear();
-    	data_vector_out.clear();
-
-    	boost::split(data_vector_string,line,boost::is_any_of(" "));
-    	for(int i =offset_node; i<data_vector_string.size();i++){
-    		if (data_vector_string[i].size() > 0){
-                double node_val;
-                std::stringstream(data_vector_string[i]) >> node_val;
-                data_vector_out.push_back(node_val/biosemi_microvoltage_factor);
-            }      
-    	}
-
-    	std::vector<double> time_sample_data(data_vector_out.begin(), data_vector_out.begin()+num_colums);
-
-    	sample_matrix.row(line_count) = arma::rowvec(time_sample_data);
-    	line_count++;
-    }
-    in.close();
-    return sample_matrix;
 }
 
 int main(int argc, char *argv[]) {
@@ -153,10 +105,7 @@ int main(int argc, char *argv[]) {
 	std::string file_name(argv[1]);
 	arma::mat sample_matrix;
 
-    if(matrix_from_moment)
-        sample_matrix.load(file_name, arma::raw_ascii);
-    else
-        sample_matrix = matrix_from_file_samples(file_name);
+    sample_matrix.load(file_name, arma::raw_ascii);
 
     auto normalization_start = std::chrono::high_resolution_clock::now();
     //arma::mat sample_matrix_t = sample_matrix.t();
